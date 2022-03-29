3760cf64d0ce12b9987c756e74442ff1bb465531
reggiepy <wangtongpy@foxmail.com>
2022-03-29 17:07:22 +0800

    (feature): add service

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # golang proxy
 
-[![go version](https://img.shields.io/github/go-mod/go-version/reggiepy/go_proxy?color=success&filename=go.mod&style=flat)]()
-[![release](https://img.shields.io/github/v/tag/reggiepy/go_proxy?color=success&label=release)]()
-[![build status](https://img.shields.io/badge/build-pass-success.svg?style=flat)]()
-[![License](https://img.shields.io/badge/license-GNU%203.0-success.svg?style=flat)]()
+[![go version](https://img.shields.io/github/go-mod/go-version/reggiepy/go_proxy?color=success&filename=go.mod&style=flat)](https://github.com/reggiepy/go_proxy)
+[![release](https://img.shields.io/github/v/tag/reggiepy/go_proxy?color=success&label=release)](https://github.com/reggiepy/go_proxy)
+[![build status](https://img.shields.io/badge/build-pass-success.svg?style=flat)](https://github.com/reggiepy/go_proxy)
+[![License](https://img.shields.io/badge/license-GNU%203.0-success.svg?style=flat)](https://github.com/reggiepy/go_proxy)
 [![Go Report Card](https://goreportcard.com/badge/github.com/reggiepy/go_proxy)](https://goreportcard.com/report/github.com/reggiepy/go_proxy)
 
 ## Installation
--- server/server.go
+++ server/server.go
@@ -1,1 +1,66 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type HandleFunc struct {
+	pattern    string
+	handleFunc http.Handler
+}
+
+type Server struct {
+	ctx        context.Context
+	host       string
+	port       string
+	handleFunc []HandleFunc
+
+	httpServer http.Server
+}
+
+func (s *Server) Start() (context.Context, error) {
+	s.registerHandleFunc()
+	ctx, cancel := context.WithCancel(s.ctx)
+	s.httpServer.Addr = fmt.Sprintf("%s:%s", s.host, s.port)
+
+	go func() {
+		err := s.httpServer.ListenAndServe()
+		if err != nil {
+			log.Println(err)
+		}
+		cancel()
+	}()
+
+	go func() {
+		fmt.Printf("Server start at: %s Press any key to stop.\n", s.httpServer.Addr)
+		var input string
+		_, _ = fmt.Scanln(&input)
+		_, _ = s.Stop(ctx)
+		cancel()
+	}()
+	return ctx, nil
+}
+
+func (s *Server) Stop(ctx context.Context) (context.Context, error) {
+	err := s.httpServer.Shutdown(ctx)
+	return ctx, err
+}
+
+func (s *Server) registerHandleFunc() {
+	for _, handleFunc := range s.handleFunc {
+		http.Handle(handleFunc.pattern, handleFunc.handleFunc)
+	}
+}
+
+func NewServer(host, port string, handleFunc []HandleFunc) *Server {
+	return &Server{
+		context.Background(),
+		host,
+		port,
+		handleFunc,
+		http.Server{},
+	}
+}
